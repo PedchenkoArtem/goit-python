5bf0cd7ee5669c8de8a4978cd713f4723cf2ed0a
PedchenkoArtem <artem.pedchenko@gmail.com>
2020-12-11 07:09:54 +0200

    hw1

--- Lesson1/hw2.py
+++ Lesson1/hw2.py
@@ -2,6 +2,8 @@ a = int (input ( 'a = '))
 b = int (input ( 'b = '))
 c = int (input ( 'c = '))
 D = b**2-4*a*c
-print (f'x1={(-b+D**0.5)/2*a}; x2={(-b-D**0.5)/2*a}')
+x1 = (-b+D**0.5)/2*a
+x2 = (-b-D**0.5)/2*a
+print ('x1=', x1, ';', 'x2=', x2)
 
 
