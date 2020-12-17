a40ff31bc7ab26f37bd00362a52bf8e95caad147
PedchenkoArtem <artem.pedchenko@gmail.com>
2020-12-17 21:56:30 +0200

    hw3

--- lesson2/hw3.py
+++ lesson2/hw3.py
@@ -1,41 +1,68 @@
-n = input("Enter the first value: ")
+user_value_1 = input("Enter the first value: ")
 
-while type(n) != float:
+while type(user_value_1) != float:
     try:
-        n = float(n) 
+        user_value_1 = float(user_value_1) 
     except ValueError:
         print("It was not a number!")
-        n = input("Enter the first value: ")
+        user_value_1 = input("Enter the first value: ")
 
-m = input("Enter an operator: ")
-if m == "+" or m == "-" or m == "*" or m == "/":
-    a = input("Enter the second value: ")
-    
-    while type(a) != float:
+user_operator = input("Enter an operator: ")
+
+if user_operator == "+" or user_operator == "-" or user_operator == "*" or user_operator == "/":
+    user_value_2 = input("Enter the second value: ")
+    while type(user_value_2) != float:
         try:
-            a = float(a)
+            user_value_2 = float(user_value_2)
         except ValueError:
             print("It was not a number!")
-            a = input("Enter the second value: ")
-    
-    b = input("Enter an equal sign: ")
-    
-    if b == "=" and m == "+":
-        result = n + a
-        print(result)
-    elif b == "=" and m == "-":
-        result = n - a
-        print(result)
-    elif b == "=" and m == "*":
-        result = n * a
-        print(result)
-    elif b == "=" and m == "/":
-        result = n / a
-        print(result)                
-    else:
-        print("It was not an equal sign!")
-        b = input("Enter an equal sign: ")
+            user_value_2 = input("Enter the second value: ")
+            while type(user_value_2) != float:
+                try:
+                    user_value_2 = float(user_value_2)
+                except ValueError:
+                    print("It was not a number!")
+                    user_value_2 = input("Enter the second value: ")
 
-else:
+elif type(user_operator) == float or type(user_operator) == str:
     print("It was not an operator!")
-    m = input("Enter an operator: ")
+    user_operator = input("Enter an operator: ")
+        
+    if user_operator == "+" or user_operator == "-" or user_operator == "*" or user_operator == "/":
+        user_value_2 = input("Enter the second value: ")
+        while type(user_value_2) != float:
+            try:
+                user_value_2 = float(user_value_2)
+            except ValueError:
+                print("It was not a number!")
+                user_value_2 = input("Enter the second value: ")
+    elif type(user_operator) == float or type(user_operator) == str:
+        print("Notice, operator is +, -, *, /. Choose one!")
+        user_operator = input("Enter an operator: ")
+        
+        if user_operator == "+" or user_operator == "-" or user_operator == "*" or user_operator == "/":
+            user_value_2 = input("Enter the second value: ")
+            while type(user_value_2) != float:
+                try:
+                    user_value_2 = float(user_value_2)
+                except ValueError:
+                    print("It was not a number!")
+                    user_value_2 = input("Enter the second value: ")
+
+user_result = input("Enter an equal sign: ")
+    
+if user_result == "=" and user_operator == "+":
+    result = user_value_1 + user_value_2
+    print(result)
+elif user_result == "=" and user_operator == "-":
+    result = user_value_1 - user_value_2
+    print(result)
+elif user_result == "=" and user_operator == "*":
+    result = user_value_1 * user_value_2
+    print(result)
+elif user_result == "=" and user_operator == "/":
+    result = user_value_1 / user_value_2
+    print(result)                
+else:
+    print("It was not an equal sign!")
+    user_result = input("Enter an equal sign: ")
